a7b70deb0fafd95d7e576690eeecba1c6d28ce77
Nicolás Ruiz <101640855+IamNewInThis@users.noreply.github.com>
2024-10-23 18:45:56 -0300

    signin

--- src/app/sign-in/sign-in.component.ts
+++ src/app/sign-in/sign-in.component.ts
@@ -1,17 +1,36 @@
 import { Component } from '@angular/core';
 import { Router, RouterModule,  } from '@angular/router';
+import { SupabaseService } from '../service/supabase.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-in',
   standalone: true,
-  imports: [RouterModule, ],
+  imports: [RouterModule, FormsModule],
   templateUrl: './sign-in.component.html',
   styleUrl: './sign-in.component.css'
 })
 export class SignInComponent {
-  constructor(private router: Router) {}
+  //Campos
+  email: string = '';
+  password: string = '';
+
+  constructor(private router: Router, private supabaseService: SupabaseService) {}
 
   goToSignUp() {
     this.router.navigate(['/signup']);
   }
+
+  // Manejar el inicio de sesión
+  async onSignIn() {
+    const { error } = await this.supabaseService.signIn(this.email, this.password);
+    if (error) {
+      console.error('Error signing in:', error.message);
+      alert('Error iniciando sesión: ' + error.message); // Mostrar mensaje de error al usuario
+    } else {
+      console.log('Signed in successfully');
+      this.router.navigate(['/home']); // Redirigir a la página principal
+    }
+  }
+  
 }
--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -1,15 +1,38 @@
 import { Component } from '@angular/core';
 import { Router, RouterModule,  } from '@angular/router';
+import { SupabaseService } from '../service/supabase.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-up',
   standalone: true,
-  imports: [RouterModule ],
+  imports: [RouterModule, FormsModule ],
   templateUrl: './sign-up.component.html',
   styleUrl: './sign-up.component.css'
 })
+
 export class SignUpComponent {
-  constructor(private router: Router) {}
+  //Campos
+  name: string = '';
+  email: string = '';
+  password: string = '';
+
+  constructor(private router: Router, private supabaseService: SupabaseService) {}
+
+  // Método para registrarse
+  async signUp() {
+    try {
+      const { error } = await this.supabaseService.signUp(this.email, this.password);
+      if (error) {
+        console.error('Error al registrarse:', error.message);
+      } else {
+        console.log('Usuario registrado exitosamente');
+        this.router.navigate(['/signin']);
+      }
+    } catch (err) {
+      console.error('Ocurrió un error:', err);
+    }
+  }
 
   goToSignIn() {
     this.router.navigate(['/signin']);
