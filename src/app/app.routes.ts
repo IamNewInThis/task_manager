39ce734aa7671573d1e9c13374908b8f1ba9af8e
Nicolás Ruiz <101640855+IamNewInThis@users.noreply.github.com>
2024-10-24 13:56:17 -0300

    sidebar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { SignInComponent } from './sign-in/sign-in.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
+import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/signin', pathMatch: 'full' },
     {path:'signup', component:SignUpComponent},
     {path:'signin', component:SignInComponent}, 
+    {path:'home', component:HomeComponent},
     { path: '**', redirectTo: '/signin' }
 ];
 
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { Router, RouterModule,  } from '@angular/router';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [RouterModule,SidebarComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
